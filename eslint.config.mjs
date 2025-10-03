944e3aca1df17b2736ce43918f24162b3cebfe91
Emil Hallberg <emil@senseworks.io>
2025-10-03 21:34:39 +0200

    Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,6 +24,54 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    files: [
+      "**/*.{js,jsx,ts,tsx}",
+      "app.{js,ts}",
+      "next.config.{js,ts}",
+      "tailwind.config.{js,ts}",
+      "eslint.config.{js,mjs}",
+    ],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "no-console": ["warn", { allow: ["error"] }],
+      "react/jsx-curly-brace-presence": [
+        "error",
+        { props: "never", children: "never", propElementValues: "always" },
+      ],
+      "react-hooks/exhaustive-deps": "error",
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"], // Packages `react` related packages come first.
+            ["^next", "^@?\\w"], // Packages `next` related packages come first.
+            ["^(@|components)(/.*|$)"], // Internal packages.
+            ["^\\u0000"], // Side effect imports.
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"], // Parent imports. Put `..` last.
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"], // Other relative imports. Put same-folder imports and `.` last.
+            ["^.+\\.?(css)$"], // Style imports.
+          ],
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { ignoreRestSiblings: true },
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-duplicate-enum-values": "off",
+    },
+  },
+  // Disable ESLint rules that would conflict with Prettier
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
