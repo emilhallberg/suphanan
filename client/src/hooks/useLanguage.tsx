e3c1f76797da6758ae2c48a5c6c255bf43041578
Emil Hallberg <emil@senseworks.io>
2025-10-03 21:20:10 +0200

    Clean slate

--- client/src/hooks/useLanguage.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from 'react-i18next';
-import { Lang } from '../utils/enums';
-
-const useLanguage = (): Lang => {
-  const { i18n } = useTranslation();
-  return i18n.language as Lang;
-};
-
-export default useLanguage;
--- client/src/resources/stylesheets/colors.tsx
+++
@@ -1,7 +0,0 @@
-const Colors = {
-  background: '#ffffff',
-  text: '#000000',
-  contrast: '#d8d8d8',
-};
-
-export default Colors;
